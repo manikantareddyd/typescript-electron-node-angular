49d41cd4f28a418a46a7b13ee587eb9a480cbcbf
Manikanta Reddy D <manikanta.reddy.d@outlook.com>
2017-07-10 10:52:02 +0530

    Directory Reorganization.

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -8,7 +8,7 @@ const tsProject = ts.createProject('tsconfig.json');
 gulp.task('scripts', () => {
   const tsResult = tsProject.src()
   .pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest('../dist/backend'));
+  return tsResult.js.pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', ['scripts'], () => {
@@ -17,7 +17,7 @@ gulp.task('watch', ['scripts'], () => {
 
 gulp.task('assets', function() {
   return gulp.src(JSON_FILES)
-  .pipe(gulp.dest('../dist/backend'));
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['watch', 'assets']);
