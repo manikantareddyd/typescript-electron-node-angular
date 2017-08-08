aff4a8a0a60fe9f9caa05f6a7bec4320ff1ab0a0
Manikanta Reddy <mareddyd@microsoft.com>
2017-08-08 10:30:36 +0530

    consolidated social strategies

--- src/server/auth/passport/passport.config.ts
+++ src/server/auth/passport/passport.config.ts
@@ -1,17 +1,25 @@
 import * as passport from 'passport';
-import facebookStrategy from './facebook.strategy';
-import twitterStrategy from './twitter.strategy';
+import {
+    facebookStrategy,
+    twitterStrategy
+} from './strategies';
+
 function configure_passport(): void {
 
     passport.use(facebookStrategy);
+
     passport.use(twitterStrategy);
 
     passport.serializeUser<any, any>((user, done) => {
+        // This is where we fuck em all
+        // No serializing or deserializing hahahahaha
         console.log("Serializing")
         return done(undefined, user.id);
     });
 
     passport.deserializeUser((id: string, done) => {
+        // This is where we fuck em all
+        // No serializing or deserializing hahahahaha
         console.log("Deserializing");
         let user = { id: id }
         return done(null, user)
---
+++ src/server/auth/passport/strategies/index.ts
@@ -0,0 +1,7 @@
+import facebookStrategy from './facebook.strategy';
+import twitterStrategy from './twitter.strategy';
+
+export {
+    facebookStrategy,
+    twitterStrategy
+}
